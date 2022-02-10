40aab1fe31dac2a182b9b6d9a0b1932982a46760
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2022-02-10 17:27:06 +0000

    Sesion 4

--- pubsub.js
+++ pubsub.js
@@ -2,27 +2,55 @@ const redis = require('redis');
 const REDIS_URL = process.env.REDIS_URL || process.env.REDIS_LOCAL_URL
 
 const CHANNELS = {
-    TEST:'TEST'
+    TEST:'TEST',
+    BLOCKCHAIN: 'BLOCKCHAIN'
 };
 
 class PubSub{
-    constructor(){
+    constructor({blockchain}){
+        this.blockchain = blockchain;
+
         this.publisher = redis.createClient(REDIS_URL);
         this.subscriber = redis.createClient(REDIS_URL);
 
-        this.subscriber.subscribe(CHANNELS.TEST);
+        this.subscribeToChannels();
 
-        this.subscriber.on('message', (channel, message) => this.handleMessage(channel, message)
+        this.subscriber.on('message', 
+        (channel, message) => this.handleMessage(channel, message)
         );
+
     }
 
     handleMessage(channel, message){
         console.log(`Message received. Channel: ${channel}. Message: ${message}.`);
+
+        const parsedMessage = JSON.parse(message);
+        if(channel === CHANNELS.BLOCKCHAIN){
+            this.blockchain.replaceChain(parsedMessage);
+        }
+    }
+
+    subscribeToChannels(){
+        Object.values(CHANNELS).forEach(channel =>{
+            this.subscriber.subscribe(channel);
+        });
+    }
+
+    publish({channel, message}){
+        this.subscriber.unsubscribe(channel,() =>{
+            this.publisher.publish(channel, message, () =>{
+                this.subscriber.subscribe(channel);
+            });
+        });
+    }
+
+    broadcastChain(){
+        this.publish({
+            channel: CHANNELS.BLOCKCHAIN,
+            message: JSON.stringify(this.blockchain.chain)
+        });
     }
 }
 
-const testPubSub = new PubSub();
-
-
-testPubSub.publisher.publish(CHANNELS.TEST, 'foo');
+module.exports = PubSub;
 
