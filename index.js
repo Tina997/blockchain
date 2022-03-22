a4467ef05ac7f8e7b5c0e9942dcfcc42df18ffbf
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2022-03-22 20:11:33 +0000

    Direccion de produccion

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const REDIS_URL = isDevelopment ?
 'redis://127.0.0.1:6379': 
 'redis://:pa8f1306ae9ab8fe1e72e11104b0579279f1dc0f810f0ea0bd82ffb46b9f87698@ec2-34-195-207-177.compute-1.amazonaws.com:11909'
 const DEFAULT_PORT = 3000;
-const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+const ROOT_NODE_ADDRESS = isDevelopment ?
+`http://localhost:${DEFAULT_PORT}`:
+`https://young-basin-12073.herokuapp.com/`;
 
 const app = express();
 const blockchain = new Blockchain();
