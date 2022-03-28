11fa0b622aef9dc73431bf99a97b877bd4fdc9ff
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2022-03-28 17:58:18 +0100

    prueba

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,5 +1,12 @@
 import React, {Component} from "react";
-
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+const template = await pool.query('SELECT * FROM test_table');
 class Block extends Component{
     render(){
         const {timestamp, hash, data} = this.props.block;
@@ -15,6 +22,7 @@ class Block extends Component{
                 <div>Hash: {hash}</div>
                 <div>Timestamp: {new Date(timestamp).toLocaleString()}</div>
                 <div>Data: {data}</div>
+                <div>Array: {template.rows}</div>
 
             </div>
         )
