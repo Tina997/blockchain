bc7e2962a09d8718a43d695d1d79eb9e9b686318
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2022-03-28 15:24:10 +0100

    prueba

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get('/db', async (req, res) => {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};*/
-      res.render(path.join(__dirname, 'client/src/db.html'),{});
+      res.render('client/src/db.html',{});
       /*client.release();
     } catch (err) {
       console.error(err);
