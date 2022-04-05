8931cab3e9efb197c4f0571d22e7a0fb4571e2cd
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2022-04-05 08:06:13 +0100

    Prueba

--- databaseModel.js
+++ databaseModel.js
@@ -4,7 +4,7 @@ const {cryptoHash} = require("./server/util");
 
 module.exports = {
     async insert(data){
-        /*let lastHash = obtainLastHash();
+        let lastHash = obtainLastHash();
         console.log('lasHash: ',lastHash);
         let timestamp = Date.now().toLocaleString();
         console.log('timestamp: ',timestamp);
@@ -13,16 +13,19 @@ module.exports = {
         console.log('difficulty: ', difficulty);
         let hash = cryptoHash(timestamp, lastHash, data, difficulty);
         console.log('hash: ', hash);
-        let results = await connection.query(`insert into block_table
+        /*let results = await connection.query(`insert into block_table
         (timestamp, lastHash, hash, difficulty, data)
         values ($1, $2, $3, $4, $5)`,[timestamp, lastHash, hash, difficulty, data]);
 
         return results;*/
 
+       /* await connection.query(`insert into block_table
+        (timestamp, lastHash, hash, difficulty, data)
+        values ('2022/02/20 7:20:00.59', 'hash-one', '----', 1, 'foo-data')`);*/
+
         await connection.query(`insert into block_table
         (timestamp, lastHash, hash, difficulty, data)
-        values ('2022/02/20 7:20:00.59', 'hash-one', '----', 1, 'foo-data')`);
-
+        values (`+timestamp+`, `+ lastHash+`, `+ hash+`, `+ difficulty+ `, `+ data`)`);
     },
     async obtainLastHash(){
         const result = await connection.query("select hash from block_table order by timestamp desc limit 1");
