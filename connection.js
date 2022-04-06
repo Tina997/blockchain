750dc45aacfd7b85d23b9f309577aec1c04d395a
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2022-04-06 13:08:00 +0100

    Archivo conexión

--- connection.js
+++ connection.js
@@ -1,7 +1,11 @@
 
 const { Pool } = require('pg');
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: 'drqcjolywcqrrg',
+    host: 'ec2-18-214-134-226.compute-1.amazonaws.com',
+    database: 'd468des2v2an2m',
+    password: 'aace6136193085d3d19247e47a11d284ecc057e8864bd3f70af0bdb93565f924',
+    port: 5432,
     ssl: {
         rejectUnauthorized: false
     }
