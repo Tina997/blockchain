9ed8e2354fcca5a9c1052114dc5bb47974607b7a
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2022-06-19 13:09:45 +0100

    Prueba

--- databaseModel.js
+++ databaseModel.js
@@ -19,8 +19,7 @@ module.exports = {
         let newBlock = Block.mineBlock(lastBlock, data);*/
         console.log("Hola");
         console.log(lastBlocks.rows[0]);
-        let timestamp1 = lastBlocks.rows[0].timestamp;
-        let lastBlock = new Block(timestamp1, lastBlocks.rows[0].lastHash,lastBlocks.rows[0].hash,lastBlocks.rows[0].difficulty,lastBlocks.rows[0].data);
+        let lastBlock = new Block(lastBlocks.rows[0].timestamp + "", lastBlocks.rows[0].lastHash,lastBlocks.rows[0].hash,lastBlocks.rows[0].difficulty,lastBlocks.rows[0].data);
         console.log(lastBlock);
         let newBlock = blockchain.addBlock(lastBlock,data);
         console.log("Adeu");
