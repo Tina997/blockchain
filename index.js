c5d13db2cccbf00c4f2179579cf5ac8618e3c08f
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2022-07-02 18:15:30 +0100

    Prueba

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
 const wallet = new Wallet();
 const pubsub = new PubSub({blockchain, transactionPool, redisUrl: REDIS_URL});
-const transactionMiner = new TransactionMiner({blockchain, transactionPool, wallet, pubsub});
-const databaseModel = new DatabaseModel();
+//const transactionMiner = new TransactionMiner({blockchain, transactionPool, wallet, pubsub});
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,'client/dist')));
