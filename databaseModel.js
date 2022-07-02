f03266d049673c20bf2216f7b57c4d46223c946d
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2022-07-02 20:09:22 +0100

    Prueba

--- databaseModel.js
+++ databaseModel.js
@@ -42,14 +42,14 @@ module.exports = {
         let hash = newBlock.hash;
         let difficulty = newBlock.difficulty;
         let data = newBlock.data;
-        result = await connection.query(`insert into block_table
+        result = connection.query(`insert into block_table
         (timestamp, lastHash, hash, difficulty, data)
         values ($1, $2, $3, $4, $5)`,[timestamp, lastHash, hash, difficulty, data]);
 
         return result;
     },
     obtainLastBlock(){
-        let lastBlocks = await connection.query("select * from block_table order by timestamp desc limit 1");
+        let lastBlocks = connection.query("select * from block_table order by timestamp desc limit 1");
         const lastBlock = new Block(lastBlocks.rows[0].timestamp,lastBlocks.rows[0].lastHash,lastBlocks.rows[0].hash,
             lastBlocks.rows[0].difficulty,lastBlocks.rows[0].data);
         return lastBlock;
